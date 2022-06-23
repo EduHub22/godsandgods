5e49ceaf0dc407acb59ad148154c8923434f1ab4
Edu Albalat <ealbalat@CLMAC0014.local>
2022-06-23 10:48:21 +0200

    first commit

---
+++ db/pdoconfig.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'ejercicio3';
+$username = 'edu';
+$password = '1234';
+
+$options = [
+    \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE  => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES  => false
+];
+
